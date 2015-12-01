bd2c77d6c5d454b5a9655c9dc19d2430619669f7
Jason Marshall <jmarshal@caltech.edu>
2015-12-01 14:37:05 -0800

    Initial setup of file structure.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pydatapub',
+      version='0.1',
+      description='Academic publishing fo media packages',
+      url='https://github.com/jason-marshall/pydatapub',
+      author='Jason Marshall',
+      author_email='jason.p.marshall@gmail.com',
+      license='MIT',
+      packages=['pydatapub'],
+      zip_safe=False)
