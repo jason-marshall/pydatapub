5a083f7b62b8e9d02651a152ba4e2b3e499b9b08
Jason Marshall <jmarshal@caltech.edu>
2016-02-09 11:39:01 -0800

    Update.

--- pydatapub/datamanip.py
+++ pydatapub/datamanip.py
@@ -16,3 +16,29 @@ def get_data(filename,
         
     return columns
 
+def rolling_average(array,
+                    average):
+
+    array_average = np.zeros(len(array))
+
+    for i in range(0,len(array)):
+        dummy = 0.0
+        count = 0
+        for j in range(i-average,i+average+1):
+            if j >= 0 and j < len(array):
+                dummy += array[j]
+                count+=1
+        dummy = dummy/float(count)
+        array_average[i] = dummy
+    return array_average
+
+def subtract_constant(array,
+                      constant):
+
+    new_array = np.zeros(len(array))
+
+    for i in range(0,len(array)):
+        new_array[i] = array[i] - constant
+
+    return new_array
+
